40d33825c225f7c72b7fa1466b8c3d53c117baeb
Setsuma <plug.veg@gmail.com>
2025-01-14 10:29:19 +0100

    fix: modify the order of Assert

--- tests/Thomas.Siest.ObjectDetection.Tests/ObjectDetectionUnitTest.cs
+++ tests/Thomas.Siest.ObjectDetection.Tests/ObjectDetectionUnitTest.cs
@@ -24,16 +24,16 @@ namespace Thomas.Siest.ObjectDetection.Tests
             var detectObjectInScenesResults = await new ObjectDetection().DetectObjectInScenesAsync(imageScenesData);
 
             Assert.Equal(
+                "[{\"Dimensions\":{\"X\":0.8776779,\"Y\":196.59088,\"Height\":199.99211,\"Width\":158.47617},\"Label\":\"dog\",\"Confidence\":0.5492805}]",
+                JsonSerializer.Serialize(detectObjectInScenesResults[1].Box)
+            );
+            Assert.Equal(
                 "[{\"Dimensions\":{\"X\":224.27759,\"Y\":64.26219,\"Height\":226.47531,\"Width\":81.26442},\"Label\":\"bottle\",\"Confidence\":0.8861316}," +
                 "{\"Dimensions\":{\"X\":128.21892,\"Y\":83.48009,\"Height\":180.8642,\"Width\":75.946815},\"Label\":\"bottle\",\"Confidence\":0.7525618}," +
                 "{\"Dimensions\":{\"X\":308.8601,\"Y\":101.06335,\"Height\":151.38896,\"Width\":58.952698},\"Label\":\"bottle\",\"Confidence\":0.33410567}]",
                 JsonSerializer.Serialize(detectObjectInScenesResults[0].Box)
             );
 
-            Assert.Equal(
-                "[{\"Dimensions\":{\"X\":0.8776779,\"Y\":196.59088,\"Height\":199.99211,\"Width\":158.47617},\"Label\":\"dog\",\"Confidence\":0.5492805}]",
-                JsonSerializer.Serialize(detectObjectInScenesResults[1].Box)
-            );
         }
 
         private static string GetExecutingPath()
